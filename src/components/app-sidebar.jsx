fcc4032777c37f891241fbef6b578bc95f17b6c5
CHIRANTH RAJU C <chiranthrajuc@gmail.com>
2025-01-07 17:45:40 +0530

    upload

--- src/components/app-sidebar.jsx
+++ src/components/app-sidebar.jsx
@@ -193,8 +193,8 @@ export function AppSidebar({
                   <Command className="size-4" />
                 </div>
                 <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-semibold">Acme Inc</span>
-                  <span className="truncate text-xs">Enterprise</span>
+                  <span className="truncate font-semibold">Prep Pandit</span>
+                  <span className="truncate text-xs">DPP</span>
                 </div>
               </a>
             </SidebarMenuButton>
